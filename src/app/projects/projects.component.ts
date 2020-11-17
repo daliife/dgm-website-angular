aba2dae965189607876bb88c8e3f5d86af5f37d5
David G <compresdavid@gmail.com>
2020-11-17 18:25:23 +0100

    fix: tabs materialize typings + change styles

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Project } from './project.model';
 import { ProjectsService } from './projects.service';
 
+declare var M: any;
+
 @Component({
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css'],
   providers: [ProjectsService],
 })
-
 export class ProjectsComponent implements OnInit {
-
   projects: Project[];
 
   githubProjects: any[];
@@ -19,9 +19,13 @@ export class ProjectsComponent implements OnInit {
   }
 
   public ngOnInit() {
-    this.projectsService.getApiProjects().subscribe(data => {
+    this.projectsService.getApiProjects().subscribe((data) => {
       this.githubProjects = data;
     });
-  }
 
+    // const tabs = document.querySelectorAll('.tabs');
+    // for (let i = 0; i < tabs.length; i++) {
+    //   M.Tabs.init(tabs[i]);
+    // }
+  }
 }
